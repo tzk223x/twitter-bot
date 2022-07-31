840700aea9031f40fff140f8c403e248377e488c
Thomas <tzk223@gmail.com>
2022-07-31 05:25:16 +0000

    add logging

---
+++ custom_streaming_client.py
@@ -0,0 +1,56 @@
+"""Custom streaming client class based of tweepy StreamingClient class"""
+
+import logging
+import pytz
+from tweepy import StreamingClient
+
+class CustomStreamingClient(StreamingClient):
+    """Custom streaming client class based of tweepy StreamingClient class"""
+
+    def __init__(self, bearer_token, discord_webhook, tweepy_client):
+        """override constructor to include discord webhook and tweepy client attributes"""
+        super().__init__(bearer_token)
+        self.discord_webhook = discord_webhook
+        self.tweepy_client = tweepy_client
+
+    def on_tweet(self, tweet):
+        """customize on_tweet action to send message to Discord"""
+        logging.info("Getting tweet link from tweet text...")
+        logging.debug({ "tweet.text": tweet.text })
+        tweet_link = tweet.text.split(" ")[-1]
+        logging.debug({ "tweet_link": tweet_link })
+
+        logging.info("Convert tweet timestamp in UTC to string in Pacific Time...")
+        logging.debug({ "tweet.created_at": tweet.created_at })
+        tweet_datetime_utc = tweet.created_at.replace(tzinfo=pytz.utc)
+        logging.debug({ "tweet_datetime_utc": tweet_datetime_utc })
+        tweet_datetime_pt = tweet_datetime_utc.astimezone(pytz.timezone("America/Los_Angeles"))
+        logging.debug({ "tweet_datetime_pt": tweet_datetime_pt })
+        tweet_datetime_pt_string = tweet_datetime_pt.strftime("%Y-%m-%d %H:%M:%S") + " PT"
+        logging.debug({ "tweet_datetime_pt_string": tweet_datetime_pt_string })
+
+        logging.info("Getting tweet author username from ID...")
+        logging.debug({ "tweet.author_id": tweet.author_id })
+        tweet_author_user = self.tweepy_client.get_user(id=tweet.author_id)
+        logging.debug({ "tweet_author_user": tweet_author_user })
+        tweet_author_username = tweet_author_user[0]["username"]
+        logging.debug({ "tweet_author_username": tweet_author_username })
+
+        discord_webhook_username = "twitter-bot"
+        discord_webhook_avatar_url = ""
+        discord_webhook_content = f"{str(tweet_author_username)}"\
+            f"tweeted at {tweet_datetime_pt_string}: {tweet_link}"
+        logging.info(
+            {
+                "discord_webhook_username": discord_webhook_username,
+                "discord_webhook_avatar_url": discord_webhook_avatar_url,
+                "discord_webhook_content": discord_webhook_content
+            }
+        )
+
+        logging.info("Sending message with Discord webhook...")
+        self.discord_webhook.send(
+            username=discord_webhook_username,
+            avatar_url=discord_webhook_avatar_url,
+            content=discord_webhook_content
+        )
--- run.py
+++ run.py
@@ -1,30 +1,88 @@
-from customStreamingClient import CustomStreamingClient
-import dotenv
-import tweepy
+"""Entry point to run Twitter bot"""
+
+import argparse
+import logging
 import os
+from discord import Webhook, RequestsWebhookAdapter
 from dotenv import load_dotenv
+import tweepy
+from custom_streaming_client import CustomStreamingClient
+
+# Importing arguments
+parser = argparse.ArgumentParser(
+    description="Runs Twitter bot",
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter
+)
+parser.add_argument(
+    "--discord-webhook-url",
+    help="Discord webhook URL",
+    type=str
+)
+parser.add_argument(
+    "--twitter-bearer-token",
+    help="Twitter API bearer token",
+    type=str
+)
+parser.add_argument(
+    "--debug",
+    action="store_true",
+    help="Set logging level to DEBUG"
+)
+args=parser.parse_args()
+
+# Create logger
+logger = logging.getLogger()
+logger.setLevel(logging.NOTSET)
+
+# Create console logger
+CONSOLE_HANDLER_FORMAT = '%(asctime)s | %(levelname)s: %(message)s'
+console_handler = logging.StreamHandler()
+if args.debug:
+    console_handler.setLevel(logging.DEBUG)
+else:
+    console_handler.setLevel(logging.INFO)
+console_handler.setFormatter(logging.Formatter(CONSOLE_HANDLER_FORMAT))
+logger.addHandler(console_handler)
 
 load_dotenv()
 
-DISCORD_WEBHOOK_URL = os.getenv('DISCORD_WEBHOOK_URL')
-TWITTER_BEARER_TOKEN = os.getenv('TWITTER_BEARER_TOKEN')
+if args.discord_webhook_url:
+    DISCORD_WEBHOOL_URL = args.discord_webhook_url
+else:
+    DISCORD_WEBHOOL_URL = os.getenv('DISCORD_WEBHOOK_URL')
 
-def main():
-    # Create tweepy streaming client
-    tweepy_client = tweepy.Client(TWITTER_BEARER_TOKEN)
-    tweepy_streaming_client = CustomStreamingClient(TWITTER_BEARER_TOKEN, DISCORD_WEBHOOK_URL, tweepy_client)
-    
-    # Clear all rules attached to account and add desired rules
+if args.twitter_bearer_token:
+    TWITTER_BEARER_TOKEN = args.twitter_bearer_token
+else:
+    TWITTER_BEARER_TOKEN = os.getenv('TWITTER_BEARER_TOKEN')
+
+def main(discord_webhook_url, twitter_bearer_token):
+    """Main routine"""
+
+    logging.info("Creating Discord webhook...")
+    discord_webhook = Webhook.from_url(discord_webhook_url, adapter=RequestsWebhookAdapter())
+
+    logging.info("Creating Tweepy streaming client...")
+    tweepy_client = tweepy.Client(twitter_bearer_token)
+    tweepy_streaming_client = CustomStreamingClient(twitter_bearer_token,
+        discord_webhook, tweepy_client)
+
+    logging.info("Clearing all rules attached to account...")
     rules = tweepy_streaming_client.get_rules()
+    logging.debug({ "rules": rules })
     if rules[0]:
         for rule in rules[0]:
             tweepy_streaming_client.delete_rules(rule.id)
+
+    logging.info("Adding desired rules...")
     #rule = tweepy.StreamRule(value="from:GenshinImpact -is:retweet")
     rule = tweepy.StreamRule(value="genshin -is:retweet -is:reply has:media")
     tweepy_streaming_client.add_rules(rule)
-    print(["Filter Rules: ",rules])
+    rules = tweepy_streaming_client.get_rules()
+    logging.debug({ "rules": rules })
 
-    # Start tweepy streaming client
+    logging.info("Starting Tweepy streaming client...")
     tweepy_streaming_client.filter(tweet_fields=["created_at"], expansions="author_id")
 
-main()
+if __name__ == "__main__":
+    main(DISCORD_WEBHOOL_URL, TWITTER_BEARER_TOKEN)
